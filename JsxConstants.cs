4eafc13ff793250c7d862a2e3976e8d953f85159
Jason Silvestri <info@jsilvestri.com>
2024-06-01 16:37:32 -0400

    Add project files.

---
+++ JsxConstants.cs
@@ -0,0 +1,136 @@
+﻿namespace JsxClassLibrary
+{
+
+    /// <summary>
+    /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all constants, across all projects included in the jSilvestri 2024 Web API Demo 
+    /// Collection projects.
+    /// </summary>
+    public static class JsxConstants
+    {
+
+        // 
+        /// <summary>
+        /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+        /// legal requirements, across all projects included in the jSilvestri 2024 Web API Demo 
+        /// Collection projects, such as copyright branding, trademark branding, contact information
+        /// and more.
+        /// </summary>
+        public static class JsxLegalParts
+        {
+            public const string JsxCopyRight = "Copyright © 2024 All Rights Reserved by Jason Silvestri";
+        }
+
+
+        /// <summary>
+        /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+        /// brand requirements, across all projects included in the jSilvestri 2024 Web API Demo 
+        /// Collection projects.
+        /// </summary>
+        public static class JsxBrand
+        {
+            public const string JsxBrandName = "jSilvestri.com";
+            public const string JsxBrandNameEncoded = "jSilvestri.com";
+            public const string JsxBrandNameAndJasonX = "jSilvestri.com - featuring JSX 2024 Web Application";
+            public const string JsxBrandNameAndJasonXEncoded = "jSilvestri.com - featuring JSX 2024 Web Application";
+        }
+
+        /// <summary>
+        /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+        /// Web API Demo Product information, across all projects included in the jSilvestri 2024 Web API Demo 
+        /// Collection projects.
+        /// </summary>
+        public static class JsxWebApiDemoProducts
+        {
+
+            /// <summary>
+            /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+            /// Web API Demo Product names, across all projects included in the jSilvestri 2024 Web API Demo 
+            /// Collection projects.
+            /// </summary>
+            public static class ProductNames
+            {
+                public const string JsxMainProjectName = "jSilvestri.com 2024 Web API Demo Collection";
+
+                public const string JsxAngularJsProject = "jSilvestri.com 2024 Web API Demo AngularJS Project";
+
+                public const string JsxBlazorProject = "jSilvestri.com 2024 Web API Demo Blazor Project";
+
+                public const string JsxReactJsProject = "jSilvestri.com 2024 Web API Demo ReactJS Project";
+
+                public const string JsxVueProject = "jSilvestri.com 2024 Web API Demo Vue Project";
+
+                public const string JsxMAUIProject = "jSilvestri.com 2024 Web API Demo MAUI Project";
+
+                public const string JsxClassLibraryProject = "jSilvestri.com 2024 Web API Demo Class Library Project";
+
+                public const string JsxWebApiProject = "jSilvestri.com 2024 Web API Demo Web API Project";
+
+            }
+
+
+            /// <summary>
+            /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+            /// Web API Demo Product descriptions, across all projects included in the jSilvestri 2024 Web API Demo 
+            /// Collection projects.
+            /// </summary>
+            public static class ProductDescriptions
+            {
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxMainProjectDescription = "The jSilvestri.com 2024 Web API Demo Collection is a series of free projects available on GitHub. These projects utilize a common Web API to get and set information based on the provided sample data. Current projects include implementations with AngularJS, Blazor, ReactJS, Vue, MAUI, and more.\r\n\r\nThis collection allows potential employers, fellow developers, and anyone interested in Jason Silvestri's expertise across various platforms, technologies, and devices to explore his work with ease.";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxClassLibraryDescription = "The JSX Class Library, is one of several applications, used to handle common Constants, Enums, Helper classes and interfaces commonly used across all jSilvestri.com 2024 Web API Demo Collection projects (available on GitHub).";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxAngularJsProjectDescription = "The JSX AngularJS Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxBlazorProjectDescription = "The JSX Blazor Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxReactJsProjectDescription = "The JSX ReactJS Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxVueProjectDescription = "The JSX Vue Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxMAUIProjectDescription = "The JSX .NET MAUI Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// </summary>
+                public const string JsxWebApiProjectDescription = "The JSX Asp.NET Core 8 Web API Project, is the official Web API from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+
+            }
+        }
+
+
+        /// <summary>
+        /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+        /// Web API Demo Product information, across all projects included in the jSilvestri 2024 Web API Demo 
+        /// Collection projects.
+        /// </summary>
+        public static class JsxWebApiDemoSettings
+        {
+
+            public const string JwtIssuer = "YourIssuer";
+            public const string JwtAudience = "YourAudience";
+            public const string JwtKey = "YourSuperSecretKey";
+        }
+
+    }
+}
