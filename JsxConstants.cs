60a5c7371c3cdc6243d812f2bb710d3321a43612
Jason Silvestri <info@jsilvestri.com>
2024-06-26 20:35:17 -0400

    06/26/2024 JSILVESTRI.COM BETA V 2024.0.0.001 Check-In
    Current Workflow State: Completed
    Mobile / Web Developer: Jason Silvestri
    
     Date Reported:    June 25, 2024
     Date Completed: June 26, 2024
     Build: BETA v 2024.0.0.001
    
     All Notes:
    This release (2024.0.0.001) marks the first iteration of the custom Official Jsx Blazor Server Asp.NET Web API Demo Application from the jSilvestri.com BETA v 2024 Web API Demo Collection.
    
    * Reverted reference chnages across all applications of the jSilvestri.com BETA v 2024 Web API Demo Collection.
    
    * Added new JsxClassLibrary Product and Name constants.

--- JsxConstants.cs
+++ JsxConstants.cs
@@ -29,6 +29,7 @@
         public static class JsxBrand
         {
             public const string JsxBrandName = "jSilvestri.com";
+            public const string JsxBrandNameAppDemos = "jSilvestri.com 2024 App Demos";
             public const string JsxBrandNameEncoded = "jSilvestri.com";
             public const string JsxBrandNameAndJasonX = "jSilvestri.com - featuring JSX 2024 Web Application";
             public const string JsxBrandNameAndJasonXEncoded = "jSilvestri.com - featuring JSX 2024 Web Application";
@@ -49,22 +50,57 @@
             /// </summary>
             public static class ProductNames
             {
-                public const string JsxMainProjectName = "jSilvestri.com 2024 Web API Demo Collection";
 
-                public const string JsxAngularJsProject = "jSilvestri.com 2024 Web API Demo AngularJS Project";
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+                /// Web API Demo Default Product names.
+                /// </summary>
+                public static class Default
+                {
+                    public const string JsxMainProjectName = "jSilvestri.com 2024 Web API Demo Collection";
 
-                public const string JsxBlazorProject = "jSilvestri.com 2024 Web API Demo Blazor Project";
+                    public const string JsxAngularJsProject = "JSX Angular Asp.NET Core Web API Demo Project";
 
-                public const string JsxReactJsProject = "jSilvestri.com 2024 Web API Demo ReactJS Project";
+                    public const string JsxBlazorProject = "JSX Blazor Asp.NET Core Web API Demo Project";
 
-                public const string JsxVueProject = "jSilvestri.com 2024 Web API Demo Vue Project";
+                    public const string JsxReactJsProject = "JSX React Asp.NET Core Web API Demo Project";
 
-                public const string JsxMAUIProject = "jSilvestri.com 2024 Web API Demo MAUI Project";
+                    public const string JsxVueProject = "JSX Vue Asp.NET Core Web API Demo Project";
 
-                public const string JsxClassLibraryProject = "jSilvestri.com 2024 Web API Demo Class Library Project";
+                    public const string JsxMAUIProject = "JSX .NET MAUI Demo Project";
 
-                public const string JsxWebApiProject = "jSilvestri.com 2024 Web API Demo Web API Project";
+                    public const string JsxClassLibraryProject = "JSX Asp.NET Core Class Library Project";
 
+                    public const string JsxWebApiProject = "JSX Asp.NET Core WebAPI Project";
+
+                    public const string JsxSharedResourcesProject = "JSX Asp.NET Shared Resources Project";
+                }
+
+
+                /// <summary>
+                /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+                /// Web API Demo Short Product names.
+                /// </summary>
+                public static class Shorts
+                {
+                    public const string JsxMainProjectName = "jSilvestri.com 2024 Demo Collection";
+
+                    public const string JsxAngularJsProject = "JSX Angular Web API Demo";
+
+                    public const string JsxBlazorProject = "JSX Blazor Web API Demo";
+
+                    public const string JsxReactJsProject = "JSX React Web API Demo";
+
+                    public const string JsxVueProject = "JSX Vue Web API Demo";
+
+                    public const string JsxMAUIProject = "JSX .NET MAUI Demo";
+
+                    public const string JsxClassLibraryProject = "JSX Class Library";
+
+                    public const string JsxWebApiProject = "JSX WebAPI";
+
+                    public const string JsxSharedResourcesProject = "JSX Shared Resources";
+                }
             }
 
 
@@ -73,48 +109,116 @@
             /// Web API Demo Product descriptions, across all projects included in the jSilvestri 2024 Web API Demo 
             /// Collection projects.
             /// </summary>
-            public static class ProductDescriptions
+            public static class Descriptions
             {
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxMainProjectDescription = "The jSilvestri.com 2024 Web API Demo Collection is a series of free projects available on GitHub. These projects utilize a common Web API to get and set information based on the provided sample data. Current projects include implementations with AngularJS, Blazor, ReactJS, Vue, MAUI, and more.\r\n\r\nThis collection allows potential employers, fellow developers, and anyone interested in Jason Silvestri's expertise across various platforms, technologies, and devices to explore his work with ease.";
 
                 /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+                /// Web API Demo Product default descriptions, across all projects included in the jSilvestri 2024 Web API Demo 
+                /// Collection projects.
                 /// </summary>
-                public const string JsxClassLibraryDescription = "The JSX Class Library, is one of several applications, used to handle common Constants, Enums, Helper classes and interfaces commonly used across all jSilvestri.com 2024 Web API Demo Collection projects (available on GitHub).";
+                public static class Default
+                {
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxMainProject = "The jSilvestri.com 2024 Web API Demo Collection is a comprehensive suite of demonstration projects designed to showcase the integration and usage of various front-end frameworks (Angular, React, Vue, Blazor) with ASP.NET Core 8 Web API. This collection serves as a learning resource and reference for developers looking to understand and implement modern web application architectures using these technologies.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxClassLibrary = "The JSX Class Library Project is a project containing reusable .NET class libraries that provide common functionalities and utilities used across different web applications within the jSilvestri.com BETA v 2024 Web API Demo Collection. This project aims to promote code reuse and maintainability by centralizing shared logic in a single location.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxWebApiProject = "The JSX Web API Project is a custom project that implements an ASP.NET Core 8 Web API, serving as the back-end for the various web applications in the jSilvestri.com BETA v 2024 Web API Demo Collection. It provides RESTful API endpoints for data retrieval and manipulation, showcasing best practices in API development and integration with front-end frameworks.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxSharedResources = "The JSX Shared Resources Project is a project that contains shared resources such as images, styles, and scripts used across multiple web applications in the jSilvestri.com BETA v 2024 Web API Demo Collection. This project aims to maintain consistency and avoid duplication of resources across different projects.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxAngularJsProject = "The JSX Angular Core Web API Demo Project is a demonstration project showcasing how to build a web application using Angular and ASP.NET Core 8 Web API. It highlights the integration of Angular for the client-side with ASP.NET Core for the server-side, providing a comprehensive example of a modern web application stack.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxBlazorProject = "The JSX Blazor Server Core Web API Demo Project is a demonstration project that illustrates how to construct a web application using Blazor Server and ASP.NET Core 8 Web API. This project highlights the use of Blazor for building interactive web UIs with C# and ASP.NET Core for providing powerful server-side capabilities.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxReactJsProject = "The JSX React Core Web API Demo Project is a demonstration project designed to illustrate how to create a web application using React and ASP.NET Core 8 Web API. This project exemplifies the combination of React for building dynamic user interfaces with ASP.NET Core for developing robust server-side APIs.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxVueProject= "The JSX Vue Core Web API Demo Project is a demonstration project intended to demonstrate how to develop a web application using Vue and ASP.NET Core 8 Web API. It showcases the seamless integration of Vue for the front-end and ASP.NET Core for the back-end, offering a clear example of a full-stack web development approach.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxMAUIProject = "The JSX .NET MAUI Core Demo Project is one coming soon that you do not want to miss. Stay tuned!";
+
+                }
 
                 /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                /// Custom jSilvestri.com 2024 Web API Demo Constants class used to handle all jSilvestri.com 2024
+                /// Web API Demo Product Short descriptions, across all projects included in the jSilvestri 2024 Web API Demo 
+                /// Collection projects.
                 /// </summary>
-                public const string JsxAngularJsProjectDescription = "The JSX AngularJS Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
-                
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxBlazorProjectDescription = "The JSX Blazor Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                public static class Short
+                {
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxMainProject = "The jSilvestri.com 2024 Web API Demo Collection is a suite of demo projects showcasing Angular, React, Vue, and Blazor with ASP.NET Core 8 Web API, serving as a reference for modern web app architectures.";
 
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxReactJsProjectDescription = "The JSX ReactJS Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxClassLibrary = "The JSX Class Library Project Contains reusable .NET class libraries providing common functionalities for various web applications, promoting code reuse and maintainability.";
 
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxVueProjectDescription = "The JSX Vue Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxWebApiProject = "The JSX Web API Project Implements an ASP.NET Core 8 Web API as the back-end for various web apps, providing RESTful endpoints and demonstrating API best practices.";
 
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxMAUIProjectDescription = "The JSX .NET MAUI Project, is one of several applications, which currently implements the Asp.NET Core 8 Web API, from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxSharedResources = "The JSX Shared Resources Project is a project that contains shared resources such as images, styles, and scripts used across multiple web applications in the jSilvestri.com BETA v 2024 Web API Demo Collection.";
 
-                /// <summary>
-                /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
-                /// </summary>
-                public const string JsxWebApiProjectDescription = "The JSX Asp.NET Core 8 Web API Project, is the official Web API from the jSilvestri.com 2024 Web API Demo Collection, for demo purposes (available on GitHub).";
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxAngularJsProject = "The JSX Angular Core Web API Demo Project demonstrates building a web app using Angular with ASP.NET Core 8 Web API, showcasing a modern web stack.";
 
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxBlazorProject = "The JSX Blazor Server Core Web API Demo Project demonstrates building a web app with Blazor Server and ASP.NET Core 8 Web API, featuring interactive UIs with server-side power.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxReactJsProject = "The JSX React Core Web API Demo Project  illustrates creating a web app with React and ASP.NET Core 8 Web API, combining dynamic UIs with robust APIs.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxVueProject = "The JSX Vue Core Web API Demo Project shows developing a web app using Vue with ASP.NET Core 8 Web API, highlighting a full-stack development approach.";
+
+                    /// <summary>
+                    /// Custom jSilvestri.com 2024 Web API Demo Constant used for product description. 
+                    /// </summary>
+                    public const string JsxMAUIProject = "The JSX .NET MAUI Core Demo Project is one coming soon that you do not want to miss. Stay tuned!";
+
+                }
             }
         }
 
